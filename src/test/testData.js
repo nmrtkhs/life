3571dc7d97a0b4c3a9cdcc3b9e5319a560c67c14
donpy <nmrtkhs@gmail.com>
2015-06-05 19:50:30 +0900

    Add test data

---
+++ src/test/testData.js
@@ -0,0 +1,47 @@
+var TestData = {
+  UserData : {
+    name : "",
+    turn : 0,
+    area : 1,
+    grid : 0,
+  },
+  AreaMaster : {
+    "1" : {
+      name : "東京",
+      bgPath : "hoge/path",
+      gridMaster : [
+        {
+          type : "minas",
+          value : 0,
+          description : "hoge1"
+        },
+        {
+          type : "minas",
+          value : 0,
+          description : "hoge2"
+        },
+        {
+          type : "minas",
+          value : 0,
+          description : "hoge3"
+        },
+        {
+          type : "minas",
+          value : 0,
+          description : "hoge4"
+        },
+        {
+          type : "minas",
+          value : 0,
+          description : "hoge5"
+        },
+        {
+          type : "minas",
+          value : 0,
+          description : "hoge6"
+        },
+      ]
+    }
+
+  },
+};
